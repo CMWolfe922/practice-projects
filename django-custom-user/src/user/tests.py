fa758b79aeda44b234aa1581d596585cff165b3a
Charles Wolfe <cmwolfe.dev@gmail.com>
2022-07-05 18:34:47 -0500

    Added a test class and some test methods for when the project is almost complete I can run multiple tests. I just have to fill in the methods

--- django-custom-user/src/user/tests.py
+++ django-custom-user/src/user/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
 # Create your tests here.
+class CreateNewUsersTest(TestCase):
+
+    def test_create_new_user(self):
+        pass
+
+    def query_user_by_name_test(self):
+        pass
+
+    def query_user_by_email_test(self):
+        pass
+
+    def update_first_name_test(self):
+        pass
+
+    def search_for_existing_user_test(self):
+        pass
